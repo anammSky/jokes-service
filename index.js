a47f5ace9f6499d9b130be82c6d235665b3413f0
Ana Moros <ana.morosmarmol@sky.uk>
2023-01-13 12:01:39 +0000

    did extra and test

--- index.js
+++ index.js
@@ -49,5 +49,64 @@ app.get("/jokes", async (req, res, next) => {
   }
 });
 
+app.get("/jokes/:id", async (req, res, next) => {
+  const joke = await Joke.findByPk(req.params.id);
+  res.send(joke);
+});
+
+app.post("/jokes", async (req, res, next) => {
+  const body = req.body;
+  const newJoke = {
+    joke: body.joke,
+    tags: Array.isArray(body.tags) ? body.tags.join(",") : body.tags,
+  };
+  try {
+    await Joke.create(newJoke);
+    res.send(201);
+  } catch (error) {
+    res.status(400).send("Unable to save joke");
+  }
+});
+
+app.delete("/jokes/:id", async (req, res, next) => {
+  const id = req.params.id;
+  // if (id < 1) {
+  //   return next(`No joke with id ${id} exists`);
+  // }
+  try {
+    await Joke.destroy({ where: { id } });
+    res.send("Joke removed");
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.put("/jokes/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const joke = await Joke.findByPk(id);
+
+  if (!joke) {
+    return next(`No joke with id ${id} exists`);
+  }
+  const body = req.body;
+  const updatedJoke = {};
+  if (body.joke) {
+    updatedJoke.joke = body.joke;
+  }
+
+  if (body.tags) {
+    updatedJoke.tags = Array.isArray(body.tags)
+      ? body.tags.join(",")
+      : body.tags;
+  }
+
+  try {
+    await joke.update(updatedJoke);
+    res.send("200");
+  } catch (error) {
+    res.status(400).send("Cannot update joke");
+  }
+});
+
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
--- index.test.js
+++ index.test.js
@@ -1,33 +1,76 @@
-const app = require('./index');
-const { sequelize, Joke } = require('./db');
-const request = require('supertest');
-const seed = require('./db/seedFn');
-const seedData = require('./db/seedData');
+const app = require("./index");
+const { sequelize, Joke } = require("./db");
+const request = require("supertest");
+const seed = require("./db/seedFn");
+const seedData = require("./db/seedData");
 
-describe('GET /jokes', () => {
-    beforeAll(async () => {
-        await sequelize.sync({ force: true }); // recreate db
-        await seed();
-    });
+describe("GET /jokes", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true }); // recreate db
+    await seed();
+  });
 
-    it('should return a list of all jokes', async () => {
-        const response = await request(app).get('/jokes');
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(seedData.length);
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[0]));
-    });
+  it("should return a list of all jokes", async () => {
+    const response = await request(app).get("/jokes");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(seedData.length);
+    expect(response.body[0]).toEqual(expect.objectContaining(seedData[0]));
+  });
 
-    it('should return a list of jokes, filtered by tag', async () => {
-        const response = await request(app).get('/jokes?tags=anatomy');
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(3);
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[3]));
-    });
+  it("should return a list of jokes, filtered by tag", async () => {
+    const response = await request(app).get("/jokes?tags=anatomy");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(3);
+    expect(response.body[0]).toEqual(expect.objectContaining(seedData[3]));
+  });
 
-    it('should return a list of jokes, filtered by content', async () => {
-        const response = await request(app).get('/jokes?content=flamingo');
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveLength(1);
-        expect(response.body[0]).toEqual(expect.objectContaining(seedData[2]));
+  it("should return a list of jokes, filtered by content", async () => {
+    const response = await request(app).get("/jokes?content=flamingo");
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveLength(1);
+    expect(response.body[0]).toEqual(expect.objectContaining(seedData[2]));
+  });
+});
+
+describe("POST /jokes", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true }); // recreate db
+    await seed();
+  });
+
+  it("should add a joke to the database", async () => {
+    const response = await request(app).post("/jokes").send({
+      joke: "What's the best thing about Switzerland? I don't know, but the flag is a big plus.",
+      tags: "countries,switzerland",
     });
+    expect(response.status).toBe(201);
+  });
+});
+
+describe("DELETE /jokes/:id", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true }); // recreate db
+    await seed();
+  });
+
+  it("should delete a joke from the database", async () => {
+    const response = await request(app).delete("/jokes/1");
+    expect(response.status).toBe(200);
+  });
+});
+
+describe("PUT /jokes/:id", () => {
+  beforeAll(async () => {
+    await sequelize.sync({ force: true }); // recreate db
+    await seed();
+  });
+
+  it("should edit a joke from the database", async () => {
+    const response = await request(app).put("/jokes/2").send({
+      joke: "Test",
+      tags: "test",
+    });
+    const newJoke = await request(app).get("/jokes/2");
+    expect(newJoke.body.joke).toEqual("Test");
+  });
 });
